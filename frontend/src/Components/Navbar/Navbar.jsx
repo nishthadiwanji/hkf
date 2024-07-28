8aca4c4b51b71b8fe0a100c7c3fd003ab7246094
nishthadiwanji <nishthadiwanji@gmail.com>
2024-07-28 19:39:50 -0400

    Pages created, routers added

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState }  from 'react'
+import {Link} from 'react-router-dom';
 import './Navbar.css'
 
 import logo from '../Assets/logo.png'
@@ -14,14 +15,26 @@ export const Navbar = () => {
                 <p>Hong Kong Fashion</p>
             </div>
             <ul className='nav-menu'>
-                <li onClick={()=>{setMenu("shop")}}>Shop {menu==="shop"?<hr/>:<></>} </li>
-                <li onClick={()=>{setMenu("men")}}>Men {menu==="men"?<hr/>:<></>} </li>
-                <li onClick={()=>{setMenu("women")}}>Women {menu==="women"?<hr/>:<></>} </li>
-                <li onClick={()=>{setMenu("kids")}}>Kids {menu==="kids"?<hr/>:<></>} </li>
+                <li onClick={()=>{setMenu("shop")}}>
+                    <Link style={{textDecoration: 'none'}} to='/'> Shop </Link> 
+                    {menu==="shop"?<hr/>:<></>} 
+                </li>
+                <li onClick={()=>{setMenu("men")}}>
+                    <Link style={{textDecoration: 'none'}} to='/men'> Men </Link>
+                    {menu==="men"?<hr/>:<></>} 
+                </li>
+                <li onClick={()=>{setMenu("women")}}>
+                    <Link style={{textDecoration: 'none'}} to='/women'> Women </Link>
+                    {menu==="women"?<hr/>:<></>} 
+                </li>
+                <li onClick={()=>{setMenu("kids")}}>
+                    <Link style={{textDecoration: 'none'}} to='/kids'> Kids </Link> 
+                    {menu==="kids"?<hr/>:<></>} 
+                </li>
             </ul>
             <div className='nav-login-cart'>
-                <button>Login</button>
-                <img src={cart_icon} alt="" />
+                <Link style={{textDecoration: 'none'}} to='/login'> <button>Login</button> </Link>
+                <Link style={{textDecoration: 'none'}} to='/cart'><img src={cart_icon} alt="" /></Link>
                 <div className="nav-cart-count">0</div>
             </div>
         </div>
